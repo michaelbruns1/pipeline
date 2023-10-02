5e880ca164823f050491588bbb6c7f4190981314
Michael Bruns <mb@michaelbruns.one>
2023-10-02 15:27:01 -0700

    First Commit with Webhelp portal

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,19 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Add",p:["p1","p2"]},{w:"a",p:["p1","p2"]},{w:"Custom",p:["p1"]},{w:"Template",p:["p1"]},{w:"Favicon",p:["p1"]},{w:"Logo",p:["p2"]},{w:"Oxygen",p:["p3"]},{w:"Webhelp",p:["p3"]},{w:"Information",p:["p4"]},{w:"Architecture",p:["p4"]},{w:"JSON",p:["p5","p7","p12"]},{w:"Schema",p:["p5","p12"]},{w:"Publishing",p:["p6"]},{w:"Templates",p:["p6"]},{w:"Validation",p:["p7","p8"]},{w:"tool",p:["p7"]},{w:"for",p:["p7"]},{w:"data",p:["p7"]},{w:"using",p:["p7","p8","p11"]},{w:"Schematron",p:["p7"]},{w:"CI/CD",p:["p8"]},{w:"Integration",p:["p8"]},{w:"WHC",p:["p9"]},{w:"Macro",p:["p9","p11"]},{w:"Michael",p:["p10"]},{w:"Bruns",p:["p10"]},{w:"Bio",p:["p10"]},{w:"Example",p:["p11"]},{w:"oxygen-webhelp-template-dir",p:["p11"]},{w:"The",p:["p12"]},{w:"2020",p:["p12"]},{w:"Notable",p:["p12"]},{w:"Changes",p:["p12"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[3, 4, 8];
+ph["p3"]=[9, 10];
+ph["p4"]=[11, 12];
+ph["p5"]=[13, 14];
+ph["p6"]=[15, 16];
+ph["p7"]=[17, 18, 19, 13, 20, 21, 22];
+ph["p8"]=[17, 21, 23, 24];
+ph["p9"]=[25, 26];
+ph["p10"]=[27, 28, 29];
+ph["p12"]=[32, 13, 14, 33, 34, 35];
+ph["p11"]=[30, 21, 31, 26];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {4:5,10:7,2:4,7:5,9:8,3:4,5:-1,6:5,13:6,12:7,11:-1,8:6};
