d4ee2b96b92a91790ee9fb4ee1d752ac3f31b2b6
Michael Bruns <mb@michaelbruns.one>
2023-10-02 19:18:06 -0700

    New Adding an Embedded Video concept added, along with a queued up video in the Git concept topic.

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,19 +1,20 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Add",p:["p1","p2"]},{w:"a",p:["p1","p2"]},{w:"Custom",p:["p1"]},{w:"Template",p:["p1"]},{w:"Favicon",p:["p1"]},{w:"Logo",p:["p2"]},{w:"Oxygen",p:["p3"]},{w:"Webhelp",p:["p3"]},{w:"Information",p:["p4"]},{w:"Architecture",p:["p4"]},{w:"JSON",p:["p5","p7","p13"]},{w:"Schema",p:["p5","p13"]},{w:"Publishing",p:["p6"]},{w:"Templates",p:["p6"]},{w:"Validation",p:["p7","p9"]},{w:"tool",p:["p7"]},{w:"for",p:["p7"]},{w:"data",p:["p7"]},{w:"using",p:["p7","p9","p12"]},{w:"Schematron",p:["p7"]},{w:"Utilizing",p:["p8"]},{w:"Version",p:["p8"]},{w:"Control",p:["p8"]},{w:"CI/CD",p:["p9"]},{w:"Integration",p:["p9"]},{w:"WHC",p:["p10"]},{w:"Macro",p:["p10","p12"]},{w:"Michael",p:["p11"]},{w:"Bruns",p:["p11"]},{w:"Bio",p:["p11"]},{w:"Example",p:["p12"]},{w:"oxygen-webhelp-template-dir",p:["p12"]},{w:"The",p:["p13"]},{w:"2020",p:["p13"]},{w:"Notable",p:["p13"]},{w:"Changes",p:["p13"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Add",p:["p1","p2"]},{w:"a",p:["p1","p2"]},{w:"Custom",p:["p1"]},{w:"Template",p:["p1"]},{w:"Favicon",p:["p1"]},{w:"Logo",p:["p2"]},{w:"Adding",p:["p3"]},{w:"an",p:["p3"]},{w:"Embedded",p:["p3"]},{w:"Video",p:["p3"]},{w:"Oxygen",p:["p4"]},{w:"Webhelp",p:["p4"]},{w:"Information",p:["p5"]},{w:"Architecture",p:["p5"]},{w:"JSON",p:["p6","p8","p14"]},{w:"Schema",p:["p6","p14"]},{w:"Publishing",p:["p7"]},{w:"Templates",p:["p7"]},{w:"Validation",p:["p8","p10"]},{w:"tool",p:["p8"]},{w:"for",p:["p8"]},{w:"data",p:["p8"]},{w:"using",p:["p8","p10","p13"]},{w:"Schematron",p:["p8"]},{w:"Utilizing",p:["p9"]},{w:"Version",p:["p9"]},{w:"Control",p:["p9"]},{w:"CI/CD",p:["p10"]},{w:"Integration",p:["p10"]},{w:"WHC",p:["p11"]},{w:"Macro",p:["p11","p13"]},{w:"Michael",p:["p12"]},{w:"Bruns",p:["p12"]},{w:"Bio",p:["p12"]},{w:"Example",p:["p13"]},{w:"oxygen-webhelp-template-dir",p:["p13"]},{w:"The",p:["p14"]},{w:"2020",p:["p14"]},{w:"Notable",p:["p14"]},{w:"Changes",p:["p14"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6, 7];
 ph["p2"]=[3, 4, 8];
-ph["p3"]=[9, 10];
-ph["p4"]=[11, 12];
-ph["p5"]=[13, 14];
-ph["p6"]=[15, 16];
-ph["p7"]=[17, 18, 19, 13, 20, 21, 22];
-ph["p8"]=[23, 24, 25];
-ph["p9"]=[17, 21, 26, 27];
-ph["p10"]=[28, 29];
-ph["p12"]=[33, 21, 34, 29];
-ph["p11"]=[30, 31, 32];
-ph["p13"]=[35, 13, 14, 36, 37, 38];
+ph["p3"]=[9, 10, 11, 12];
+ph["p4"]=[13, 14];
+ph["p5"]=[15, 16];
+ph["p6"]=[17, 18];
+ph["p7"]=[19, 20];
+ph["p8"]=[21, 22, 23, 17, 24, 25, 26];
+ph["p9"]=[27, 28, 29];
+ph["p10"]=[21, 25, 30, 31];
+ph["p12"]=[34, 35, 36];
+ph["p11"]=[32, 33];
+ph["p14"]=[39, 17, 18, 40, 41, 42];
+ph["p13"]=[37, 25, 38, 33];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {9:5,4:5,8:6,12:-1,3:4,13:7,10:8,7:5,11:7,14:6,5:-1,6:5,2:4};
+var linkToParent = {2:5,15:7,4:5,7:6,5:6,3:5,6:-1,14:8,10:6,12:8,11:9,8:6,13:-1,9:7};
