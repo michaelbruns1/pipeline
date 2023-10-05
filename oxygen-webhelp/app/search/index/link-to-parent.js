c4748b4f855dfd010c40c59b74f63f1f697cdc31
Michael Bruns <mb@michaelbruns.one>
2023-10-04 21:07:06 -0700

    Updated site

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {34:-1,30:27,27:26,19:23,26:-1,23:26,20:23,21:23,33:29,25:27,36:27,32:30,35:29,29:26,31:26,24:26};
+var linkToParent = {30:27,27:26,19:23,26:-1,23:26,20:23,21:23,33:29,25:27,36:27,32:30,35:29,29:26,31:26,24:26};
