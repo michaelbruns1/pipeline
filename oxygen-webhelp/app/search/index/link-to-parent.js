f6281b1f3eaf1f43ff2644dcbf69f9cfe05b78d0
Michael Bruns <mb@michaelbruns.one>
2023-10-05 18:03:30 -0700

    Re-added the files to fix the links

---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {11:-1,2:4,15:10,12:10,20:10,4:10,19:13,13:10,18:-1,8:-1,14:10,3:4,9:-1,10:-1,5:13,6:-1,7:10,17:13,16:10,1:4};
