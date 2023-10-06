7341ace8b0394d956ec5d76ec588b864cf01505b
Michael Bruns <mb@michaelbruns.one>
2023-10-05 17:56:44 -0700

    Updated Website Links and added blogs

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Add",p:["p1","p2"]},{w:"a",p:["p1","p2","p6"]},{w:"Custom",p:["p1"]},{w:"Template",p:["p1"]},{w:"Favicon",p:["p1"]},{w:"Logo",p:["p2"]},{w:"Adding",p:["p3"]},{w:"an",p:["p3"]},{w:"Embedded",p:["p3"]},{w:"Video",p:["p3"]},{w:"Advantages",p:["p4"]},{w:"of",p:["p4"]},{w:"Oxygen",p:["p4","p5"]},{w:"Styles",p:["p4"]},{w:"Basket",p:["p4"]},{w:"Webhelp",p:["p5"]},{w:"Creating",p:["p6"]},{w:"Github",p:["p6"]},{w:"Pages",p:["p6"]},{w:"site",p:["p6"]},{w:"Information",p:["p7"]},{w:"Architecture",p:["p7"]},{w:"JSON",p:["p8","p11","p17"]},{w:"Schema",p:["p8","p17","p18"]},{w:"Migrating",p:["p9"]},{w:"to",p:["p9"]},{w:"DITA",p:["p9"]},{w:"and",p:["p9"]},{w:"Refactoring",p:["p9"]},{w:"Publishing",p:["p10"]},{w:"Templates",p:["p10"]},{w:"Validation",p:["p11","p13"]},{w:"tool",p:["p11"]},{w:"for",p:["p11"]},{w:"data",p:["p11"]},{w:"using",p:["p11","p13","p16"]},{w:"Schematron",p:["p11"]},{w:"Utilizing",p:["p12"]},{w:"Version",p:["p12"]},{w:"Control",p:["p12"]},{w:"CI/CD",p:["p13"]},{w:"Integration",p:["p13"]},{w:"WHC",p:["p14"]},{w:"Macro",p:["p14","p16"]},{w:"Michael",p:["p15"]},{w:"Bruns",p:["p15"]},{w:"Bio",p:["p15"]},{w:"Example",p:["p16","p18"]},{w:"oxygen-webhelp-template-dir",p:["p16"]},{w:"The",p:["p17"]},{w:"2020",p:["p17"]},{w:"Notable",p:["p17"]},{w:"Changes",p:["p17"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Add",p:["p1","p2"]},{w:"a",p:["p1","p2","p6","p19"]},{w:"Custom",p:["p1"]},{w:"Template",p:["p1"]},{w:"Favicon",p:["p1"]},{w:"Logo",p:["p2"]},{w:"Adding",p:["p3"]},{w:"an",p:["p3"]},{w:"Embedded",p:["p3"]},{w:"Video",p:["p3"]},{w:"Advantages",p:["p4"]},{w:"of",p:["p4"]},{w:"Oxygen",p:["p4","p5"]},{w:"Styles",p:["p4"]},{w:"Basket",p:["p4"]},{w:"Webhelp",p:["p5"]},{w:"Creating",p:["p6"]},{w:"Github",p:["p6"]},{w:"Pages",p:["p6"]},{w:"site",p:["p6"]},{w:"Information",p:["p7"]},{w:"Architecture",p:["p7"]},{w:"JSON",p:["p8","p11","p17","p22"]},{w:"Schema",p:["p8","p17","p21"]},{w:"Migrating",p:["p9"]},{w:"to",p:["p9"]},{w:"DITA",p:["p9"]},{w:"and",p:["p9"]},{w:"Refactoring",p:["p9"]},{w:"Publishing",p:["p10"]},{w:"Templates",p:["p10"]},{w:"Validation",p:["p11","p13"]},{w:"tool",p:["p11"]},{w:"for",p:["p11"]},{w:"data",p:["p11"]},{w:"using",p:["p11","p13","p16"]},{w:"Schematron",p:["p11"]},{w:"Utilizing",p:["p12"]},{w:"Version",p:["p12"]},{w:"Control",p:["p12"]},{w:"CI/CD",p:["p13"]},{w:"Integration",p:["p13"]},{w:"WHC",p:["p14"]},{w:"Macro",p:["p14","p16"]},{w:"Michael",p:["p15"]},{w:"Bruns",p:["p15"]},{w:"Bio",p:["p15"]},{w:"Example",p:["p16","p21"]},{w:"oxygen-webhelp-template-dir",p:["p16"]},{w:"The",p:["p17"]},{w:"2020",p:["p17"]},{w:"Notable",p:["p17"]},{w:"Changes",p:["p17"]},{w:"Controlling",p:["p18"]},{w:"topichead",p:["p18"]},{w:"behavior",p:["p18"]},{w:"Nesting",p:["p19"]},{w:"Style",p:["p19"]},{w:"Tag",p:["p19"]},{w:"within",p:["p19"]},{w:"Document",p:["p19","p20"]},{w:"Asset",p:["p20"]},{w:"Management",p:["p20"]},{w:"(DAM)",p:["p20"]},{w:"Form",p:["p22"]},{w:"Subject",p:["p23"]},{w:"Scheme",p:["p23"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6, 7];
@@ -11,14 +11,19 @@ ph["p7"]=[23, 24];
 ph["p8"]=[25, 26];
 ph["p9"]=[27, 28, 29, 30, 31];
 ph["p10"]=[32, 33];
+ph["p21"]=[50, 26];
+ph["p20"]=[63, 64, 65, 66];
 ph["p12"]=[40, 41, 42];
+ph["p23"]=[68, 69];
 ph["p11"]=[34, 35, 36, 25, 37, 38, 39];
+ph["p22"]=[25, 67];
 ph["p14"]=[45, 46];
 ph["p13"]=[34, 38, 43, 44];
 ph["p16"]=[50, 38, 51, 46];
 ph["p15"]=[47, 48, 49];
-ph["p18"]=[50, 26];
+ph["p18"]=[56, 57, 58];
 ph["p17"]=[52, 25, 26, 53, 54, 55];
+ph["p19"]=[59, 60, 61, 62, 4, 63];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
